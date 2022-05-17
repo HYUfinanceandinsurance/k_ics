07f8bee341f64878bd72acde1b0708028ea431c5
inkyoseo <104630349+inkyoseo@users.noreply.github.com>
2022-05-17 12:56:46 +0900

    Update K-ICS_analysis CDVine (J=1000, true=Gumbel cop, 4 dist, mid cor, N=120).R

--- K-ICS_analysis CDVine (J=1000, true=Gumbel cop, 4 dist, mid cor, N=120).R
+++ K-ICS_analysis CDVine (J=1000, true=Gumbel cop, 4 dist, mid cor, N=120).R
@@ -35,13 +35,13 @@ prd120.vstd <- rep(NA, J)
 prd120.vemp <- rep(NA, J)
 prd120.varc <- rep(NA, J)
 prd120.velp <- rep(NA, J)
-prd120.cvine <- rep(NA, J)
+prd120.rvine <- rep(NA, J)
 
 prd120.var_std <- 0
 prd120.var_emp <- 0
 prd120.var_arc <- 0
 prd120.var_elp <- 0
-prd120.var_CVine <- 0
+prd120.var_RVine <- 0
 
 for (i in 1:J) {
   set.seed(i)
@@ -154,22 +154,22 @@ for (i in 1:J) {
   prv120.risk_cred <- prv120.risk_cred  - mean(prv120.risk_cred)
   prv120.risk_mkt  <- exp(qnorm(prv120.cop[,4], sd   =est120.parm_mkt[ 2], mean=est120.parm_mkt[ 1]))
   prv120.risk_mkt <- prv120.risk_mkt - mean(prv120.risk_mkt)
-  pcv120.risk_total <- prv120.risk_life + prv120.risk_pnc + prv120.risk_cred + prv120.risk_mkt
+  prv120.risk_total <- prv120.risk_life + prv120.risk_pnc + prv120.risk_cred + prv120.risk_mkt
   
   prd120.vstd[i]  <- sqrt(prd120.var_marginal %*% (diag(4)*0.75+0.25) %*% prd120.var_marginal)
   prd120.vemp[i]  <- quantile(sam120.risk_total, probs=0.995)
   prd120.varc[i]  <- quantile(pad120.risk_total, probs=0.995)
   prd120.velp[i]  <- quantile(pld120.risk_total, probs=0.995)
-  prd120.rvine[i] <- quantile(pcv120.risk_total, probs=0.995)
+  prd120.rvine[i] <- quantile(prv120.risk_total, probs=0.995)
   
   prd120.var_std  <- prd120.var_std + sqrt(prd120.var_marginal %*% (diag(4)*0.75+0.25) %*% prd120.var_marginal)/J
   prd120.var_emp  <- prd120.var_emp + quantile(sam120.risk_total, probs=0.995)/J
   prd120.var_arc  <- prd120.var_arc + quantile(pad120.risk_total, probs=0.995)/J
   prd120.var_elp  <- prd120.var_elp + quantile(pld120.risk_total, probs=0.995)/J
-  prd120.var_RVine <- prd120.var_RVine + quantile(pcv120.risk_total, probs=0.995)/J
+  prd120.var_RVine <- prd120.var_RVine + quantile(prv120.risk_total, probs=0.995)/J
 }
 
-c(prd120.var_std, prd120.var_emp, prd120.var_arc, prd120.var_elp, prd120.var_CVine)
+c(prd120.var_std, prd120.var_emp, prd120.var_arc, prd120.var_elp, prd120.var_RVine)
 c(prd120.var_std, prd120.var_emp, prd120.var_arc, prd120.var_elp, prd120.var_RVine) - pop.var_true
 
 
